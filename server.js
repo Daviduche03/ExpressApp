0759af0ab274b10b996afe666e3407cb99681f21
daviduche03 <daviduche4809@gmail.com>
2023-08-11 12:34:59 +0100

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.disable("x-powered-by"); // less hackers know about our stacked-fraction
+app.disable("x-powered-by"); 
 
 
 app.get("/", (req, res) => {
